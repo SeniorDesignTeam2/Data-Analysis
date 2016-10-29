e3ab8ed77495426c588880b29ee0732c89473291
lshen28 <lshen28@its.jnj.com>
2016-10-29 16:33:25 -0400

    use DKM method in RF predict DelayorNot good result can be use for time horizone next step -overscale the delay trial compare result, compile AUC, explore more option with coreModle -try delay category, and duration (again)

--- InfomationGain.R
+++ InfomationGain.R
@@ -9,8 +9,8 @@ testIdxs = data[-dt,]
 # build random forests model with certain parameters
 # setting maxThreads to 0 or more than 1 forces utilization of several processor cores
 modelRF <- CoreModel(PI.Delay.Cat~ TRIAL.PHASE+NUMBER.OF.SITES+NUMBER.OF.COUNTRIES+NUMBER.OF.SUBJECTS+PROJECT.STATUS.CODE+PROJECT.FUNDING.CODE+RESPONSIBLE.UNIT.CODE+PROJECT.TYPE.CODE+PROJECT.SUBTYPE.CODE+PROJECT.PHASE.CODE+DEVELOPMENT.CATEGORY.CODE+DISEASE.AREA.SUBCATEGORY.CODE+STRATEGIC.PRIORITY.CODE+IDP.PTRS+PRODUCT.TYPE.CODE+PRODUCT.TYPE.SUBCATEGORY.CODE, trainIdxs, model="rf",
-                     selectionEstimator="MDL",minNodeWeightRF=5,
-                     rfNoTrees=1000, maxThreads=4)
+                     selectionEstimator="DKM",minNodeWeightRF=5,
+                     rfNoTrees=1000, maxThreads=10)
 print(modelRF) # simple visualization, test also others with function plot
 
 pred <- predict(modelRF, testIdxs, type="both") # prediction on testing set

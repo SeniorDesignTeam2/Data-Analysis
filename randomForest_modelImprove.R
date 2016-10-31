54ca74ce8b038dc8f46cf7c2e772be5338b0a8b2
lshen28 <lshen28@its.jnj.com>
2016-10-31 00:24:38 -0400

    balance dataset

--- randomForest_modelImprove.R
+++ randomForest_modelImprove.R
@@ -2,11 +2,24 @@ library(randomForest)
 library(mlbench)
 library(caret)
 
+library(unbalanced)
+
 data <- read.csv("D:/Imputed_Full_Data_2.0.csv",header=TRUE)
-dt = sort(sample(nrow(data),nrow(data)*0.8))
-train = data[dt,]
-test = data[-dt,]
-tree = randomForest(PI.Delay.Binary~TRIAL.PHASE+NUMBER.OF.SITES+NUMBER.OF.COUNTRIES+NUMBER.OF.SUBJECTS+PROJECT.STATUS.CODE+PROJECT.FUNDING.CODE+RESPONSIBLE.UNIT.CODE
+n= ncol(data)
+output=factor (data$PI.Delay.Binary2)
+input=data[1:33]
+data<-ubBalance(X= input, Y=output, type="ubSMOTE", percOver=300, percUnder=150, verbose=TRUE)
+#iterate throught different value from <50% and value choose base on cross validate with method below choose highest AUC, sensitivity and specfic
+#try 260,160 to get less sample data size
+
+balancedData<-cbind(data$X,data$Y)
+colnames(balancedData)[34]="Delay.or.Not"
+
+dt = sort(sample(nrow(balancedData),nrow(balancedData)*0.8))
+train = balancedData[dt,]
+test = balancedData[-dt,]
+
+tree = randomForest(Delay.or.Not~TRIAL.PHASE+NUMBER.OF.SITES+NUMBER.OF.COUNTRIES+NUMBER.OF.SUBJECTS+PROJECT.STATUS.CODE+PROJECT.FUNDING.CODE+RESPONSIBLE.UNIT.CODE
                     +PROJECT.TYPE.CODE+PROJECT.SUBTYPE.CODE+PROJECT.PHASE.CODE+DEVELOPMENT.CATEGORY.CODE+DISEASE.AREA.SUBCATEGORY.CODE+STRATEGIC.PRIORITY.CODE+IDP.PTRS
                     +PRODUCT.TYPE.CODE+PRODUCT.TYPE.SUBCATEGORY.CODE+Dosage.Form.Description,ntree=2000,data=train)
 print(tree)

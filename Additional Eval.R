58ea1514b911d2cacdcc2b98158ef6be3ef5a7e0
lshen28 <lshen28@its.jnj.com>
2016-11-20 00:17:42 -0500

    feature value accessment over time horizon, information gain use corelearn, proof to be more dependable and more scaleable with higher computing power machine. Code did not include for loop for time horizone, but it is doable with 20 core machine over 12 hours of running time

--- Additional Eval.R
+++ Additional Eval.R
@@ -9,6 +9,6 @@ testIdxs = data[-dt,]
 
 attriEval=attrEval(PI.Delay.Cat~TRIAL.PHASE+NUMBER.OF.SITES+NUMBER.OF.COUNTRIES+NUMBER.OF.SUBJECTS+PROJECT.STATUS.CODE+PROJECT.FUNDING.CODE+RESPONSIBLE.UNIT.CODE
                    +PROJECT.TYPE.CODE+PROJECT.SUBTYPE.CODE+PROJECT.PHASE.CODE+DEVELOPMENT.CATEGORY.CODE+DISEASE.AREA.SUBCATEGORY.CODE+STRATEGIC.PRIORITY.CODE+IDP.PTRS
-                   +PRODUCT.TYPE.CODE+PRODUCT.TYPE.SUBCATEGORY.CODE+Dosage.Form.Description, data, estimator="GainRatio", costMatrix = NULL, outputNumericSplits=FALSE)
+                   +PRODUCT.TYPE.CODE+PRODUCT.TYPE.SUBCATEGORY.CODE+Dosage.Form.Description, data, estimator="InfGain", costMatrix = NULL, outputNumericSplits=FALSE)
 infoCore(what = "attrEval")
 print (attriEval)
